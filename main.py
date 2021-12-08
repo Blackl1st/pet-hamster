2957409c445fa01b5435490f29095cbbb762ee34
Blackl1st <Blackl1st@users.noreply.github.com>
2021-12-08 15:36:24 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def chew_icon():
+    basic.show_leds("""
+        . # . # .
+                . . . . .
+                . . # # #
+                . . # . #
+                . . # # #
+    """)
+    music.play_tone(147, music.beat(BeatFraction.WHOLE))
+    basic.show_leds("""
+        . # . # .
+                . . . . .
+                . . . . .
+                . . # # .
+                . . # # .
+    """)
 def sleepy_blink_icon():
     basic.show_leds("""
         . . . . .
@@ -14,6 +30,16 @@ def sleepy_icon():
                 . . # # .
                 . . # # .
     """)
+
+def on_button_pressed_a():
+    chew_icon()
+    chew_icon()
+    chew_icon()
+    basic.show_icon(IconNames.HAPPY)
+    soundExpression.giggle.play_until_done()
+    default_state()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def default_state():
     basic.show_leds("""
         . . . . .
@@ -27,10 +53,125 @@ def sleepy_blink():
     sleepy_blink_icon()
     return
 
+def on_button_pressed_b():
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(587, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(587, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(587, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.play_tone(247, music.beat(BeatFraction.HALF))
+    music.play_tone(247, music.beat(BeatFraction.HALF))
+    music.play_tone(587, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.WHOLE))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(349, music.beat(BeatFraction.HALF))
+    music.play_tone(392, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(622, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(466, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.WHOLE))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(294, music.beat(BeatFraction.HALF))
+    music.play_tone(622, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(466, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.WHOLE))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(277, music.beat(BeatFraction.HALF))
+    music.play_tone(622, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(466, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.WHOLE))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(262, music.beat(BeatFraction.HALF))
+    music.play_tone(622, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(466, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.WHOLE))
+    music.play_tone(440, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.WHOLE))
+    music.play_tone(311, music.beat(BeatFraction.HALF))
+    music.play_tone(370, music.beat(BeatFraction.HALF))
+    music.play_tone(415, music.beat(BeatFraction.HALF))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def on_gesture_shake():
     basic.show_icon(IconNames.SAD)
     soundExpression.sad.play_until_done()
-    default_state()
+    basic.show_icon(IconNames.SAD)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_logo_pressed():
@@ -51,3 +192,11 @@ sleepy_icon()
 sleepy_blink_icon()
 sleepy_icon()
 default_state()
+
+def on_every_interval():
+    default_state()
+    basic.show_icon(IconNames.SAD)
+    soundExpression.sad.play_until_done()
+    basic.show_string("Feed Me!")
+    basic.show_icon(IconNames.SAD)
+loops.every_interval(60000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function chew_icon() {
+    basic.showLeds(`
+        . # . # .
+                . . . . .
+                . . # # #
+                . . # . #
+                . . # # #
+    `)
+    music.playTone(147, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        . # . # .
+                . . . . .
+                . . . . .
+                . . # # .
+                . . # # .
+    `)
+}
+
 function sleepy_blink_icon() {
     basic.showLeds(`
         . . . . .
@@ -18,6 +36,14 @@ function sleepy_icon() {
     `)
 }
 
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    chew_icon()
+    chew_icon()
+    chew_icon()
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+    default_state()
+})
 function default_state() {
     basic.showLeds(`
         . . . . .
@@ -34,10 +60,124 @@ function sleepy_blink() {
     return
 }
 
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(277, music.beat(BeatFraction.Half))
+    music.playTone(277, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Whole))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Whole))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.playTone(277, music.beat(BeatFraction.Half))
+    music.playTone(277, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Whole))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Whole))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+})
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     basic.showIcon(IconNames.Sad)
     soundExpression.sad.playUntilDone()
-    default_state()
+    basic.showIcon(IconNames.Sad)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     basic.showIcon(IconNames.Happy)
@@ -58,3 +198,10 @@ sleepy_icon()
 sleepy_blink_icon()
 sleepy_icon()
 default_state()
+loops.everyInterval(60000, function on_every_interval() {
+    default_state()
+    basic.showIcon(IconNames.Sad)
+    soundExpression.sad.playUntilDone()
+    basic.showString("Feed Me!")
+    basic.showIcon(IconNames.Sad)
+})
